9df64c9a021b15f280168a81064cbe09e0d7397d
anon_real <anon_real_anon@protonmail.com>
2024-10-20 13:11:12 -0700

    Ready to be used as sdk

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-export * from './serializer';
+// Export all public modules and functions
 export * from './gluon';
+export * from './txUtils';
+export * from './gluonBox';
+export * from './serializer';
 export * from './nodeService';
+export * from './goldOracleBox';
+
+// You can also export specific items if needed
+// export { SomeClass } from './someFile';
+// export { someFunction } from './anotherFile';
